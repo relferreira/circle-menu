a663870f9af856d9aa0dac7366e9d54df8dea9bc
Renan Ferreira <relferreira@quatroimpar.com>
2013-11-03 09:43:21 -0200

    Primeiro commit - Inserindo arquivos

---
+++ circle-menu.js
@@ -0,0 +1,31 @@
+(function($){
+	$.fn.circle_menu = function(options){
+		var settings = $.extend({
+			angle_initial: 0,
+			angle_increment:60,
+			radius:60
+		}, options);
+		var flag_click = false;		
+		var angle = (settings.angle_initial*Math.PI)/180;
+		var angle_increment = (settings.angle_increment*Math.PI)/180;
+		var icon_central = $(this).children("span");
+		var icon_circle = $(this).children("li");
+		icon_central.on("click",function(){
+			// $("#teste li").addClass('ativo');	
+			if(!flag_click){
+				angle_aux = angle;				
+				radius = settings.radius
+				flag_click = true;				
+			} else {
+				radius = 0;
+				flag_click = false;
+			}			
+			icon_circle.each(function(){
+				left = radius*Math.cos(angle_aux);
+				bottom =radius*Math.sin(angle_aux);		
+				$(this).animate({'left':left,'bottom':bottom},{duration:500});
+				angle_aux+=angle_increment;			
+			})
+		})
+	}
+}(jQuery));
---
+++ example.js
@@ -0,0 +1,7 @@
+$(window).ready(function(){
+	jQuery('.circle-menu').circle_menu({
+		angle_initial: -50,
+		angle_increment:20,
+		radius:150		
+	});
+})
