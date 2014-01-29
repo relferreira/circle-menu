31b28743b909cfda655b2baf28cfa45b169e22a6
Renan Ferreira <relferreira@quatroimpar.com>
2014-01-28 23:37:49 -0200

    better exemple

--- example.js
+++ example.js
@@ -1,7 +1,19 @@
 $(window).ready(function(){
 	jQuery('.circle-menu').circle_menu({
-		angle_initial: -50,
-		angle_increment:20,
-		radius:150		
+		angle_initial: -70,
+		angle_increment:60,
+		radius:90		
 	});
+	jQuery("#menu").on("click",function(){
+		var img = $(this).find("img");
+		if(img.attr("class")=="fechar"){
+			img.removeClass("fechar");
+			img.addClass("abrir");
+		}
+		else{
+			img.removeClass("abrir");
+			img.addClass("fechar");
+		}
+		
+	})
 })
